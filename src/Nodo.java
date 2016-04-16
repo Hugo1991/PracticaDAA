44ebe4b5b7139010325f90d7136adc66d8f53bd8
Hugo1991 <hugofernandezvisier@gmail.com>
2016-04-16 12:42:57 +0200

    creado nodo y fichero

--- src/Nodo.java
+++ src/Nodo.java
@@ -1,4 +1,58 @@
 
 public class Nodo {
+	private int beneficio;
+	private int beneficioO;
+	private int paso;
+	private int[] PastelerosAsignados;
+	private int[][] TablaBenecifios;
+
+	public int getBeneficio() {
+		return beneficio;
+	}
+
+	public void setBeneficio(int beneficio) {
+		this.beneficio = beneficio;
+	}
+
+	public int getBeneficioO() {
+		return beneficioO;
+	}
+
+	public void setBeneficioO(int beneficioO) {
+		this.beneficioO = beneficioO;
+	}
+
+	public int getPaso() {
+		return paso;
+	}
+
+	public void setPaso(int paso) {
+		this.paso = paso;
+	}
+
+	public int[] getPastelerosAsignados() {
+		return PastelerosAsignados;
+	}
+
+	public void setPastelerosAsignados(int[] pastelerosAsignados) {
+		PastelerosAsignados = pastelerosAsignados;
+	}
+
+	public int[][] getTablaBenecifios() {
+		return TablaBenecifios;
+	}
+
+	public void setTablaBenecifios(int[][] tablaBenecifios) {
+		TablaBenecifios = tablaBenecifios;
+	}
+
+	public Nodo(int beneficio, int beneficioO, int paso, int[] pastelerosAsignados, int[][] tablaBenecifios) {
+		super();
+		this.beneficio = beneficio;
+		this.beneficioO = beneficioO;
+		this.paso = paso;
+		PastelerosAsignados = pastelerosAsignados;
+		TablaBenecifios = tablaBenecifios;
+	}
 
 }
--- src/Pasteleria.java
+++ src/Pasteleria.java
@@ -1,10 +1,18 @@
 import java.io.IOException;
 
 public class Pasteleria {
-	
+	static int numPasteleros;
+	static int numPasteles;
+	static int[][] pastelero_pastel;
+	static int[] pedido;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Fichero file=new Fichero();
+		file.leerFichero("entrada.txt");
+		numPasteleros=file.getNumPasteleros();
+		numPasteles=file.getNumPasteles();
+		pastelero_pastel=file.getPastelero_pastel();
+		pedido=file.getPedido();
 	}
 
 }
